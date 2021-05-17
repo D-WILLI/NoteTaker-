fcabd024718851e50a5688096fde02a945fdec77
Diamond <williams.diamond2392@gmail.com>
2021-05-16 23:48:42 -0400

    fixing the notes file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
 
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
